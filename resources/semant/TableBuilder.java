4507e0ab6d0ceb67ced916c61781a0f7d5de317a
epln60 <eric.peulings@mni.thm.de>
2018-06-14 10:01:22 +0200

    TableBuilder Erste Änderung

--- resources/semant/TableBuilder.java
+++ resources/semant/TableBuilder.java
@@ -8,18 +8,23 @@ import absyn.*;
 class TableBuilder {
 
 	private boolean showTables;
+	private Table symTable = new Table();
 
 	Table buildSymbolTables(Absyn program, boolean showTables) {
+		TableBuilderVisitor visitor = new TabBuilderVisitor();
+		program.accept(visitor);
 	}
     
 	private class TableBuilderVisitor extends DoNothingVisitor {
-
+		private Type resultType;
 		public void visit(DecList list) {
-
+			for(Dec dec: list){
+				dec.accept(this);
+			}
 		}
 
 		public void visit(TypeDec node) {
-
+			
 		}
 
 		public void visit(NameTy node) {
@@ -39,7 +44,8 @@ class TableBuilder {
 		}
 
 		public void visit(VarDec node) {
-
+			node.ty.accept(this);
+			symTable.enter(node.name, new VarEntry(resultType, false));
 		}
 	}
 }
