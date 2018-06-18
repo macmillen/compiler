cc1c5944c39e62d35fb248780bcd7ef8d8702138
epln60 <eric.peulings@mni.thm.de>
2018-06-18 14:51:00 +0200

    TableBuilder Changes

--- resources/semant/TableBuilder.java
+++ resources/semant/TableBuilder.java
@@ -3,7 +3,11 @@ package semant;
 import sym.Sym;
 import table.*;
 import types.*;
+
+import java.util.Iterator;
+
 import absyn.*;
+import jdk.jfr.internal.Type;
 
 class TableBuilder {
 
@@ -24,23 +28,35 @@ class TableBuilder {
 		}
 
 		public void visit(TypeDec node) {
-			
+			node.ty.accept(this);
+			symTable.enter(node.name, new TypeEntry(node.ty));
 		}
 
 		public void visit(NameTy node) {
-
+			node.ty.accept(this);
+			
 		}
 
 		public void visit(ArrayTy node) {
-
+			node.baseTy.accept(this);
+            resultType = new ArrayType(node.size, resultType);
 		}
 
 		public void visit(ProcDec node) {
-
+			node.ty.accept(this);
+			ParamTypeList ptList = new ParamTypeList();
+			int index = 0;
+			ListNodeIterator pIt = node.params.iterator();
+			while(node.params.iterator().hasNext()){
+				ptList.add(index, new ParamType(pIt.next(), r));
+				index ++;
+			}
+			symTable.enter(node.name, new ProcEntry(ptList, symTable));
 		}
 
 		public void visit(ParDec node) {
-
+			node.ty.accept(this);
+			resultType = new ParamType(node.ty, node.isRef);
 		}
 
 		public void visit(VarDec node) {
@@ -49,5 +65,6 @@ class TableBuilder {
 		}
 	}
 }
+
 
 
